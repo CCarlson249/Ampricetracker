e83923be60772c03842133ff4d7bd5dc0ffbc0cf
Clayton Carlson <claytonrc97@gmail.com>
2023-10-31 23:46:05 -0700

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className='px-6 md:px-20 py-24 border-2 border-red-500'>
+              <div className='flex max-xl:flex-col gap-16'>
+                  <div className='flex flex-col justify-center'>
+                      <p className='small-text'>
+                        Smart Shopping Starts Here:
+                        <Image src='/assets/icons/arrow-right.svg' alt='arrow-right' width={16} height={16} />
+                      </p>
+                      <h1 className='head-text'>
+                        Unleash the Power of <span className='text-primary'>PriceTracker</span>
+                      </h1>
+
+                      <p className='mt-6'>
+                        Monitor your favorite products prices and availability
+                      </p>
+                  </div>
+              </div>
+      </section>
+    </>
   )
 }
 
